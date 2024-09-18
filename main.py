36ba15413ff698eb0aa6453e9e5f76c534c4c2f2
xtiao <1332575979@qq.com>
2024-09-18 14:47:49 +0800

    V2.0

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-
+import re
 import threading
-import time
 import cv2
-
+import time
 from pynput.mouse import  Controller, Button
 import mss
 import numpy as np
 from pynput import keyboard
 import win32gui
-from winerror import NOERROR
+
 
 COLORS=[(0, 0, 0), (17, 34, 51), (34, 68, 102), (51, 102, 153), (68, 136, 204), (85, 170, 255), (102, 204, 50), (119, 238, 101), (136, 16, 152), (153, 50, 203), (170, 84, 254), (187, 118, 49), (204, 152, 100), (221, 186, 151), (238, 220, 202), (255, 254, 253)]
-
+count=0
 mouse = Controller()
 flag=True
-speed=0.001
-
+speed=9
 
 def wait_dnf_process(process_name='地下城与勇士'):
     while True:
@@ -26,9 +24,6 @@ def wait_dnf_process(process_name='地下城与勇士'):
         else:
             print(f"waiting for {process_name}...")
             time.sleep(2)
-def add(a,b):
-    return tuple(i+j for i,j in zip(a,b))
-
 def _mss(game_region):
     monitor = {
         "left": game_region[0],
@@ -42,13 +37,18 @@ def _mss(game_region):
         return cv2.cvtColor(np.array(sct.grab(monitor)), cv2.COLOR_BGRA2BGR)[top:bottom, left:right]
 
     return shot
+def microsecond_sleep(sleep_time):
+    sleep_time *= 1000
+    end_time = time.perf_counter() + (sleep_time - 0.9) / 1e6  # 0.8是时间补偿，需要根据自己PC的性能去实测
+    while time.perf_counter() < end_time:
+        pass
 def clicked(x,y):
     mouse.position = (x, y)
-    time.sleep(speed)
+    microsecond_sleep(speed)
     mouse.press(Button.left)
-    time.sleep(speed)
+    microsecond_sleep(speed)
     mouse.release(Button.left)
-    time.sleep(speed)
+    microsecond_sleep(speed)
 def kb():
 # 监听键盘键入
     with keyboard.Events() as events:
@@ -59,7 +59,15 @@ def kb():
                 flag=False
                 break
         time.sleep(0.2)
+
+def get_xy():
+    with keyboard.Events() as _events:
+        for _event in _events:
+            if isinstance(_event, keyboard.Events.Release) and _event.key.char == 'd':
+                return Controller().position
+
 def start(xy):
+    global count
     tg=(xy['identify_txy'][0]-1,xy['identify_txy'][1]-1,xy['identify_bxy'][0]+1,xy['identify_bxy'][1]+1)
     black_w=(xy['region_bxy'][0]-xy['region_txy'][0])//16
     black_h=(xy['region_bxy'][1]-xy['region_txy'][1])//12
@@ -77,24 +85,28 @@ def start(xy):
         try:
             key = color_xy[(tuple(img[1, 1]), tuple(img[h-1, w-1]))]
         except KeyError:
-            clicked(*xy['end'])
-            time.sleep(0.2)
-            clicked(*xy['start'])
+            count += 1
+            if count > 40:
+                time.sleep(0.2)
+                clicked(*xy['end'])
+                time.sleep(0.1)
+                clicked(*xy['start'])
+                mouse.press(Button.left)
+                mouse.release(Button.left)
+                count = 0
             continue
-
         clicked(*xy['identify_txy'])
         clicked(xy['region_txy'][0]+key[0],xy['region_txy'][1]+key[1])
-def get_xy():
-    with keyboard.Events() as _events:
-        for _event in _events:
-            if isinstance(_event, keyboard.Events.Release) and _event.key.char == 'd':
-                return Controller().position
+        count = 0
+
+
 
 if __name__ == '__main__':
     print('-务必已管理员身份打开脚本-')
+    print('-------------------------')
     print('S键-自动校准位置并开始脚本,')
     print('A键-手动校准位置并开始脚本')
-    print('D键-修改速度')
+    print('D键-修改速度(鼠标在拼图上闪烁请调大速度)')
     print('Esc键-退出脚本')
     base_xy={
         'start':(850,770),
@@ -105,42 +117,40 @@ if __name__ == '__main__':
         'region_bxy':(1173,580),
     }
 
-
     with keyboard.Events() as events:
         for event in events:
             try:
                 if isinstance(event, keyboard.Events.Release) and event.key.char=='a':
+                    print('-------------------------------------')
                     print('请依次录入,鼠标移动到指定位置后按D键')
-                    print('-------------------------')
-                    print('选择拼图')
+                    print('-------------------------------------')
+                    print('> 请将鼠标移动到','“选择拼图”','按钮上,按D键 <')
                     base_xy['start']=get_xy()
                     base_xy['end'] = base_xy['start']
-                    print('色块上')
+                    print('> 请将鼠标移动到','“色块上”','后按D键 <')
                     base_xy['identify_txy'] = get_xy()
-                    print('色块下')
+                    print('> 请将鼠标移动到','“色块下”','后按D键 <')
                     base_xy['identify_bxy'] = get_xy()
-                    print('拼图左上')
+                    print('> 请将鼠标移动到','“拼图左上”','后按D键 <')
                     base_xy['region_txy'] = get_xy()
-                    print('拼图右下')
+                    print('> 请将鼠标移动到','“拼图右下”','后按D键 <')
                     base_xy['region_bxy'] = get_xy()
                     print('完成')
                     break
                 elif  isinstance(event, keyboard.Events.Release) and event.key.char=='s':
                     lt = wait_dnf_process()
                     for k,v in base_xy.items():
-                        base_xy[k]=add(lt,base_xy[k])
+                        base_xy[k]=tuple(i + j for i, j in zip(lt,base_xy[k]))
                     break
                 elif  isinstance(event, keyboard.Events.Release) and event.key.char=='d':
-
-                    print(f'请输入:当前({speed},回车确认)')
-                    speed=float(input()[1:])
-                    print('已设置',speed)
+                    print(f'当前{speed}毫秒,输入后回车确认:')
+                    speed=float(re.sub(r'[a-zA-Z]', '', input()))
+                    print('已设置',speed,'ms')
                     print('S键自动校准位置,A键手动校准位置,D键修改速度,Esc键退出脚本')
                 elif event.key == keyboard.Key.esc:
                     exit(0)
             except AttributeError:
                 pass
-
     print('开始执行,Esc键退出脚本...')
     start(base_xy)
     print('脚本退出')
